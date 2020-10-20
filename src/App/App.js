8c84777a91c6469cb8c035c62360070f9c40a5d2
GrafSoul <grafsoul@gmail.com>
2020-10-20 22:23:00 +0500

    feat: Added Fonts to the project

--- src/App/App.js
+++ src/App/App.js
@@ -4,7 +4,6 @@ import Aux from './hoc/AuxComponent/AuxComponent';
 import Header from './components/Header/Header';
 import Player from './components/Player/Player';
 
-
 class App extends Component {
     render() {
         return (
